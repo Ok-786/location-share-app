b778ceff3490229a5beb9d17bdaf566feb474e5a
Ok-786 <osamakiyani6123@gmail.com>
2021-10-03 19:06:59 +0500

    adaedwe

--- src/views/NewPlace.js
+++ src/views/NewPlace.js
@@ -47,7 +47,7 @@ const NewPlace = () => {
       formData.append('address', formState.inputs.address.value);
       formData.append('image', formState.inputs.image.value);
 
-      await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places`,
+      await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/`,
         'POST',
         formData,
         {
